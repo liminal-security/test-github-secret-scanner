86ef3c61642124a4887425dd2cab1f99ab77c107
Nitzan Benshimol <nitzan.benshimol@liminal.security>
2023-05-29 15:54:26 +0300

    SD

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("ghp_Mer10tpExrmwtypOhsF4CXNhOsp2fB3qQcoP")
-	fmt.Printf("AKIARYHSWWJH2APEZTYF")
-	fmt.Printf("0KDdPr3hRcNHjhPsMvUp7TcDCyHJp06Q1lwaLRty")
-	fmt.Printf("2fa2962a-9f49-417b-8912-afb79941a7q5")
-	fmt.Printf("hvs.Yi723NiLqZsSEISgrYVlzwq5")
+	fmt.Printf("ghp_Mer10tpSnrmwtypOhsD8CXNhOsp2fB3qQcoP")
+	fmt.Printf("AKIARYHCWRJF2TPDZTYF")
+	fmt.Printf("0KDdPw3hRcNHjhPjMvUp7TcDCyHJp06W1lwaLRty")
+	fmt.Printf("2fa2962a-9f49-417b-8912-bhr79941a7q5")
+	fmt.Printf("hvs.Yi753NiLqZsDGISgrYVhzwq5")
+	fmt.Printf("ghp_Mer10tpSnrmwtypOhsD8CXNhOsp2fB3qHdfG")
 }
