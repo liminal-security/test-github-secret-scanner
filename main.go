fda81478d519f465cd06e9fb2a8b5b9c57ae32cb
Nitzan Benshimol <nitzan.benshimol@liminal.security>
2023-05-29 13:52:07 +0300

    sd'

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("ghp_Mer10tpExrmwtypOhsF4CXNhOsp2fB3qQcoP")
+	fmt.Printf("AKIARYHSWWJH2APEZTYF")
+	fmt.Printf("0KDdPr3hRcNHjhPsMvUp7TcDCyHJp06Q1lwaLRty")
+	fmt.Printf("2fa2962a-9f49-417b-8912-afb79941a7q5")
+	fmt.Printf("hvs.Yi723NiLqZsSEISgrYVlzwq5")
+}
