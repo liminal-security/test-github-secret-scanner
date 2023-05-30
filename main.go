bd6428ef7fb7bd2867519f70777b38a7bbd1d37f
Nitzan Benshimol <nitzan.benshimol@liminal.security>
2023-05-30 17:46:37 +0300

    SD

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 	fmt.Printf("2fa2962a-9f49-417b-4456-bhr79941a7q5")
 	fmt.Printf("hvs.Yi753NiLqZsDGISgrYVhzwq5")
 	fmt.Printf("ghp_Mer10tpSnrmwtypOhsD8CXNhOsp2fB3qHdfG")
-	fmt.Printf("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")
+	fmt.Printf("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")
 }
